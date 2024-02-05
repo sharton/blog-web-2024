bb09067c2e08dc5d6a798267285e3305d8d52bb7
Andrew Sharton <sharton.web@gmail.com>
2024-02-05 16:49:25 +0600

    added post_detail, changed template

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', post_list, name='post_list'),
+    path('<int:pk>/', post_detail, name='post_detail'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+#
+#
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
